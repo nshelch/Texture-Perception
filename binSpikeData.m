69384796f2e42c50daabe965e787d73c13343d38
Natalya Shelchkova <nshelch@gmail.com>
2024-01-23 18:30:55 -0600

    Spike trains are no longer stitched together
    And some minor stylistic edits

--- binSpikeData.m
+++ binSpikeData.m
@@ -9,22 +9,21 @@ spikeTrain = zeros(numNeurons, numTextures, numReps, numData - 1);
 for nn = 1:numNeurons
     for tt = 1:numTextures
         for rr = 1:numReps
-            
-            spikesInMs = data.neuron(nn).texture(tt).rep{rr} * 1000;
+            % Get the spikes that occurred during the period of interest
+            % and align them to 0
+            spikesInMs = data.neuron(nn).texture(tt).rep{rr} * 1000; % Convert to ms
             timeEnd = startTime + poi;
             validSpikes = spikesInMs >= startTime & spikesInMs <= timeEnd;
             spikeTimes = spikesInMs(validSpikes) - startTime; % Align to 0
             binnedSpikeTrain = histcounts(spikeTimes, 0:binRes:poi); % Bin the spike times 
 
-            % Binarize the data (CHECK THIS WAS DONE CORRECTLY (SINCE I
-            % MOVED SHIT SO QUICKLY BEFORE THE MEETING)
-            binarizedSpikes = binnedSpikeTrain > 1;
-            binnedSpikeTrain(binarizedSpikes) = 1;
-            spikeTrain(nn, tt, rr, :) = binnedSpikeTrain;
-
+            % Binarize the data
+            multSpikeIdx = binnedSpikeTrain > 1; % Find the indices where more than one spike landed in a bin
+            binarizedSpikeTrain = binnedSpikeTrain; % Copy the binned data
+            binarizedSpikeTrain(multSpikeIdx) = 1; % Set bins with  multiple spikes equal to 1
             
+            spikeTrain(nn, tt, rr, :) = binarizedSpikeTrain;
         end % rep loop
-        
     end % texture loop
 end % neuron loop
 
