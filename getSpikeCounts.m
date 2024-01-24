3eef7b2f9160502d0de9102c17039e785f6e8bb3
Natalya Shelchkova <nshelch@gmail.com>
2024-01-23 18:33:42 -0600

    Spike count edits
    Changed the function to handle the no longer stitched together trials, not sure about the intTime renaming (might rename it later to more accurately reflect what it means).

--- getSpikeCounts.m
+++ getSpikeCounts.m
@@ -1,8 +1,13 @@
-function [spikeCount] = getSpikeCounts(spikeTrain, snippetLength)
+function [spikeCount] = getSpikeCounts(spikeTrain, intTime)
+% TODO: rename intTime to better represent what it actually means
+% TODO: Should I have this output p(x|t) instead of spike counts (with
+% p(x|t) calculated in the calculateMutualInformation fx)
+% Outputs the spike counts (determined by the integration window [intTime])
 
 numNeurons = size(spikeTrain, 1);
 numTextures = size(spikeTrain, 2);
-numDataPerNeuron = size(spikeTrain, 3) / snippetLength;
+numReps = size(spikeTrain, 3);
+numDataPerNeuron = size(spikeTrain, 4) / intTime;
 
 if ~(floor(numDataPerNeuron) == ceil(numDataPerNeuron))
     error('Current parameters will result in uneven spike count binning.')
@@ -15,9 +20,15 @@ for nn = 1:numNeurons
     fprintf(repmat('\b', 1, pbar))
     pbar = fprintf('Getting Spike Counts for neuron %i/%i \n', nn, numNeurons);
     for tt = 1:numTextures
-        spikeCount(nn, tt, :) = sum(reshape(spikeTrain(nn, tt, :), [snippetLength, numDataPerNeuron])', 2);
-    end
-end
+        tmpSpikeCount = cell(1, numReps);
+        for rr = 1:numReps
+            % Reshaping the binarized spike train for easier spike counting
+            spikeMatrix = reshape(spikeTrain(nn, tt, rr, :), [intTime, numDataPerNeuron])';
+            tmpSpikeCount{rr} = sum(spikeMatrix, 2);
+        end % rep loop
+        spikeCount(nn, tt, :) = vertcat(tmpSpikeCount{:});
+    end % texture loop
+end % neuron loop
 
 
 end
