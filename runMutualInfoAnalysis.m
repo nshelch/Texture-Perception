dc032f3ee84a69a414ece395b800d98ee9153ff6
Natalya Shelchkova <nshelch@gmail.com>
2024-01-23 18:29:02 -0600

    Home PC Edits
    Added code to recognize my home PC and some formatting for later

--- runMutualInfoAnalysis.m
+++ runMutualInfoAnalysis.m
@@ -8,16 +8,16 @@ if strcmpi(hostEnv, 'DESKTOP-LEG2SE6')
     pathLoc = 'C:/Users/nshel/';
 elseif strcmpi(hostEnv, 'OBA-PC-01')
     pathLoc = 'C:/Users/somlab/';
-else % For my at home PC
-
+elseif strcmpi(hostEnv, 'DESKTOP-FB47T9U')
+    pathLoc = 'C:/Users/nshelch/';
 end
 
-dataLoc = fullfile(pathLoc, '/Box/BensmaiaLab/Texture Perception/Data/');
+dataLoc = fullfile(pathLoc, '/Box/BensmaiaLab (Natalya Shelchkova)/Texture Perception/Data/');
 
 % Load Data
 if exist(fullfile(dataLoc, 'cData.mat'), 'file')
     load(fullfile(dataLoc, 'cData.mat'))
-    load(fullfile(dataLoc, 'dissimData.mat'))
+    %     load(fullfile(dataLoc, 'dissimData.mat'))
 else
     load(fullfile(dataLoc, 'cdaData.mat'))
     cData = formatData(cdaData.fullRun.data);
@@ -33,7 +33,7 @@ saveDataFilename = './Data/infoCalculations_ISI_SpikeCount_IntervalISI_DistMetri
 startTime = 100; % ms
 poiDur = 1800; % Duration of period of interest (poi) in ms
 convFactor = 1000; % Conversion factor from bits/bin to bits/sec in MI calculations (1000 keeps the info in units of bits/bin)
-intTimeWindow = [1, 2, 5, 10, 20, 25, 50, 100]; % Time period (ms) over which a neuron can receive information (integration time)
+intTimeWindow = 5; %[1, 2, 5, 10, 20, 25, 50, 100]; % Time period (ms) over which a neuron can receive information (integration time)
 
 % Spike Train Params
 spikeTrainRes = 1; % Binning resolution of the spike train (ms)
@@ -45,11 +45,12 @@ stParams.snippetLength = snippetLength; stParams.spikeTrainRes = spikeTrainRes;
 isiRes = 1; % ms
 isiCutoff = 120; %ms
 
+isiParams.resolution = isiRes; isiParams.cutoff = isiCutoff;
+
 % Tau Params
 isiTau = 1:15; % 1:19
 maxTauCount = [19,9,6,4,3,3,2,2,2,1,1,1,1,1,1]; % [9,4,3,2,1,1,1,1,1]; %
 
-isiParams.resolution = isiRes; isiParams.isiTimeWindow = isiParams.isiTimeWindow; isiParams.cutoff = isiCutoff;
 isiParams.tau = isiTau; isiParams.maxTauRate = maxTauCount;
 
 %% Processing Loop
@@ -59,57 +60,75 @@ isiParams.tau = isiTau; isiParams.maxTauRate = maxTauCount;
 % calculateMutualInformation (since that already takes joint distributions
 % as input)
 
+%% ISI calculations using actual spike times (gonna keep it for now but might remove later)
 % Get ISI Data
-isiEdges = 0:isiRes:isiCutoff; % 0:1:120
-isiHist = getISIHist(cData, startTime, poiDur, isiRes, isiCutoff);
+% isiEdges = 0:isiRes:isiCutoff; % 0:1:120
+% isiHist = getISIHist(cData, startTime, poiDur, isiRes, isiCutoff);
+%
+% for ll = 1:length(intTimeWindow) % This should be extended to the outer most loop (since we will do this for all the info calcs)
+%     clc; fprintf('ISI Res: %i/%i, ISI Length %i/%i\n',  length(isiRes), ll, length(intTimeWindow));
+%
+%     isiLimit = find(isiEdges <= intTimeWindow(ll), 1, 'last');
+%
+%     tmpISI = calculateMutualInformation(isiHist(:,:,1:isiLimit), NaN, convFactor);
+%     miNeural.ISI(ll, :) = tmpISI.mutualInfo;
+%     entNeural.ISI(ll, :) = tmpISI.entropy;
+%     jointNeural.ISI{ll} = tmpISI.jointCount;
+%     %         rate.ISI(ll, :, :) =
+%     %         [distMetric.ISI.pcDJS(ll, :, :, :), distMetric.ISI.pcRsq(ll, :)] = djsAnalysis(tmpISI.jointCount(find(pcIdx == 1), textIdx, :), humanScores, 0);
+%     %         [distMetric.ISI.saDJS(ll, :, :, :), distMetric.ISI.saRsq(ll, :)] = djsAnalysis(tmpISI.jointCount(find(saIdx == 1), textIdx, :), humanScores, 0);
+% end
 
-for ll = 1:length(intTimeWindow)
-    clc; fprintf('ISI Res: %i/%i, ISI Length %i/%i\n',  length(isiRes), ll, length(intTimeWindow));
+%% Info calculations using binarized spike trains
+% TODO: Implement integration time window here
+spikeTrain = binSpikeData(cData, startTime, poiDur, spikeTrainRes);
 
-    isiLimit = find(isiEdges <= intTimeWindow(ll), 1, 'last');
-
-    tmpISI = calculateMutualInformation(isiHist(:,:,1:isiLimit), NaN, convFactor);
+for ii = 1:length(intTimeWindow)
+    % Spike Count
+    for sl = 1:length(snippetLength)
+        numBins = snippetLength(sl) / spikeTrainRes;
+        
+        % Spike Count Stuff
+        [spikeCount] = getSpikeCounts(spikeTrain, numBins); % Outputs spike count not p(x|t)
+        
+        tmpSpikes = calculateMutualInformation(spikeCount, 0:numBins, convFactor);
+        miNeural.SpikeCount(sl, :) = tmpSpikes.mutualInfo;
+        entNeural.SpikeCount(sl, :) = tmpSpikes.entropy;
+        jointNeural.SpikeCount{sl} = tmpSpikes.jointCount;
+        rate.SpikeCount(sl, :, :) = mean(spikeCount, 3);
+        %         [distMetric.SpikeCount.pcDJS(sl, :, :, :), distMetric.SpikeCount.pcRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(pcIdx == 1), textIdx, :), humanScores, 1);
+        %         [distMetric.SpikeCount.saDJS(sl, :, :, :), distMetric.SpikeCount.saRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(saIdx == 1), textIdx, :), humanScores, 0);
+        
+    end % snippet loop
+    
+    % ISI
+    isiEdges = 0:isiRes:isiCutoff; % 0:1:120
+    
+    [isiCount] = getISITau(spikeTrain, snippetLength(sl), isiTau(tt));
+    tmpISI = calculateMutualInformation(isiCount, NaN, convFactor);
     miNeural.ISI(ll, :) = tmpISI.mutualInfo;
     entNeural.ISI(ll, :) = tmpISI.entropy;
     jointNeural.ISI{ll} = tmpISI.jointCount;
-    %         rate.ISI(ll, :, :) =
-    %         [distMetric.ISI.pcDJS(ll, :, :, :), distMetric.ISI.pcRsq(ll, :)] = djsAnalysis(tmpISI.jointCount(find(pcIdx == 1), textIdx, :), humanScores, 0);
-    %         [distMetric.ISI.saDJS(ll, :, :, :), distMetric.ISI.saRsq(ll, :)] = djsAnalysis(tmpISI.jointCount(find(saIdx == 1), textIdx, :), humanScores, 0);
-end
-
-%% TODO: Implement integration time window here
-spikeTrain = binSpikeData(cData, startTime, poiDur, spikeTrainRes);
-for sl = 1:length(snippetLength)
-    fprintf('Calculating Tau and Spike Count for Snippet Length %i/%i\n', sl, length(snippetLength));
-
-    % Calculate ISI Tau
-    for tt = 1:length(isiTau)
-        fprintf('Calculating Tau %i/%i\n', tt, length(isiTau));
-        [isiCount] = getISITau(spikeTrain, snippetLength(sl), isiTau(tt));
-
-        tmpTauIsi = calculateMutualInformation(isiCount, 0:maxTauCount(tt), convFactor);
-        miNeural.TauIsi(tt, :) = tmpTauIsi.mutualInfo;
-        entNeural.TauIsi(tt, :) = tmpTauIsi.entropy;
-        jointNeural.TauIsi{tt} = tmpTauIsi.jointCount;
-        rate.isiTau(tt, :) = mean(squeeze(mean(isiCount, 2)), 2);
+    
+    
+    % Spike Interval Code (was ISI Tau)
+    for sl = 1:length(snippetLength)
+        fprintf('Calculating Tau and Spike Count for Snippet Length %i/%i\n', sl, length(snippetLength));
+        
+        % Calculate ISI Tau
+        for tt = 1:length(isiTau)
+            fprintf('Calculating Tau %i/%i\n', tt, length(isiTau));
+            [spikeIntCount] = getISITau(spikeTrain, snippetLength(sl), isiTau(tt));
+            
+            tmpTauIsi = calculateMutualInformation(spikeIntCount, 0:maxTauCount(tt), convFactor);
+            miNeural.TauIsi(tt, :) = tmpTauIsi.mutualInfo;
+            entNeural.TauIsi(tt, :) = tmpTauIsi.entropy;
+            jointNeural.TauIsi{tt} = tmpTauIsi.jointCount;
+            rate.isiTau(tt, :) = mean(squeeze(mean(spikeIntCount, 2)), 2);
+        end
     end
-end
-
-for sl = 1:length(snippetLength)
-    numBins = snippetLength(sl) / spikeTrainRes(ss);
-
-    % Spike Count Stuff
-    [spikeCount] = getSpikeCounts(spikeTrain, numBins);
-
-    tmpSpikes = calculateMutualInformation(spikeCount, 0:numBins, convFactor);
-    miNeural.SpikeCount(sl, :) = tmpSpikes.mutualInfo;
-    entNeural.SpikeCount(sl, :) = tmpSpikes.entropy;
-    jointNeural.SpikeCount{sl} = tmpSpikes.jointCount;
-    rate.SpikeCount(sl, :, :) = mean(spikeCount, 3);
-    %         [distMetric.SpikeCount.pcDJS(sl, :, :, :), distMetric.SpikeCount.pcRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(pcIdx == 1), textIdx, :), humanScores, 1);
-    %         [distMetric.SpikeCount.saDJS(sl, :, :, :), distMetric.SpikeCount.saRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(saIdx == 1), textIdx, :), humanScores, 0);
-
-end % snippet loop
+    
+end % integration time window loop
 
 if saveData
     save('./Data/mostRecentCalculation.mat', 'miNeural', 'entNeural', 'jointNeural', 'distMetric', 'stParams', 'isiParams', 'neuronType', 'pcIdx', 'saIdx', 'isiHist', 'spikeTrain')
