1d96cb9644a9384d7ce7418f7d0616f0231dbca6
Natalya Shelchkova <nshelch@gmail.com>
2024-01-29 06:30:31 -0600

    Spike interval conditions in progress

--- getSpikeIntervalCount.m
+++ getSpikeIntervalCount.m
@@ -10,7 +10,7 @@ if ~(floor(numDataPerRep) == ceil(numDataPerRep))
     error('Current parameters will result in uneven binning.')
 end
 
-spikeIntCount = zeros(numNeurons, numTextures, numDataPerNeuron);
+spikeIntCount = cell(numNeurons, numTextures, numDataPerNeuron);
 
 pbar = fprintf('Getting Spike Interval Count for neuron 0/%i \n', numNeurons); % progress bar
 
@@ -21,9 +21,13 @@ for nn = 1:numNeurons
         for rr = 1:numReps
             spikeMatrix = reshape(spikeTrain(nn, tt, rr, :), [intTime, numDataPerRep])';
             for ii = 1:numDataPerRep
-                spikeIdx = find(spikeMatrix(ii, :));
-                spikeIntValue = diff(spikeIdx);
-                if ~isempty(spikeIntValue) && length(spikeIntValue) > 1
+                spikeIdx = find(spikeMatrix(ii, :)); % Find the spike idx
+                spikeIntValue = diff(spikeIdx); % Get the difference for the spike interval code
+                
+                % Spike Interval Conditions
+                if isempty(spikeIdx) % no spikes 
+                    spikeIntCount(nn, tt, ii) = {0};
+                elseif ~isempty(spikeIntValue) && length(spikeIntValue) > 1 % two spikes or more
                     spikeIntCount(nn, tt, ii) = sum(spikeIntValue == tau);
                 end
             end
