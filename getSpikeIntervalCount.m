056014e17998edd5ade3848ad0e52c221c42f950
Natalya Shelchkova <nshelch@gmail.com>
2024-02-06 19:21:08 -0600

    Updated spike int count
    No more fake first spike, but runs hella slow

--- getSpikeIntervalCount.m
+++ getSpikeIntervalCount.m
@@ -1,9 +1,9 @@
-function [spikeIntCount] = getSpikeIntervalCount(spikeTrain, intTime)
+function [spikeIntCount] = getSpikeIntervalCount(spikeTrain, snippetLength)
 
 numNeurons = size(spikeTrain, 1);
 numTextures = size(spikeTrain, 2);
 numReps = size(spikeTrain, 3);
-numDataPerRep = size(spikeTrain, 4) / intTime;
+numDataPerRep = size(spikeTrain, 4) / snippetLength;
 numDataPerNeuron = numDataPerRep * numReps;
 
 if ~(floor(numDataPerRep) == ceil(numDataPerRep))
@@ -11,41 +11,42 @@ if ~(floor(numDataPerRep) == ceil(numDataPerRep))
 end
 
 spikeIntCount = cell(numNeurons, numTextures, numDataPerNeuron);
-tmpspikeIntCount = zeros(numNeurons, numTextures, numDataPerNeuron, length(0:intTime)); % In this case, each row is the ISI interval (starting at 0) 
+spikeIntTable = zeros(numNeurons, numTextures, numDataPerNeuron, length(0:(snippetLength - 1))); % In this case, each row is the spike interval (starting at 0)
 
 pbar = fprintf('Getting Spike Interval Count for neuron 0/%i \n', numNeurons); % progress bar
 
 curBinIdx = 1; % counter for which data bin we are in (since reps are no longer stitched together, avoiding confusion with how to mix the rr and ii labels.
 for nn = 1:numNeurons
-    fprintf(repmat('\b', 1, pbar))
-    pbar = fprintf('Getting Spike Interval Count for neuron %i/%i \n', nn, numNeurons);
     for tt = 1:numTextures
         for rr = 1:numReps
-            spikeMatrix = reshape(spikeTrain(nn, tt, rr, :), [intTime, numDataPerRep])';
+            fprintf(repmat('\b', 1, pbar))
+            pbar = fprintf('Getting Spike Interval Count for Neuron: %i/%i Texture: %i Rep: %i\n', nn, numNeurons, tt, rr);
+            
+            spikeMatrix = reshape(spikeTrain(nn, tt, rr, :), [snippetLength, numDataPerRep])';
             
             % These steps might be vectorizable but lets not for now
+            
             for ii = 1:numDataPerRep
-                spikeIdx = find(spikeMatrix(curBinIdx, :)); % Find the spike idx
-%                 spikeIntValue = diff(spikeIdx); % Get the difference for the spike interval code 
-                spikeIntValue = diff([0, spikeIdx]); % Get the difference for the spike interval code (assumes there is a spike right before the bins)
-               
+                spikeIdx = find(spikeMatrix(ii, :)); % Find the spike idx
+                spikeIntValue = diff(spikeIdx); % Get the difference for the spike interval code
+                
                 % Spike Interval Conditions
                 if isempty(spikeIdx) % no spikes
                     spikeIntCount(nn, tt, curBinIdx) = {0};
-                    tmpspikeIntCount(nn, tt, curBinIdx, 1) = 1;
-
-                elseif length(spikeIdx) == 1 % 1 spike only
-                    spikeIntCount(nn, tt, curBinIdx) = {spikeIdx};
-                    tmpspikeIntCount(nn, tt, curBinIdx, spikeIdx + 1) = 1; % We add 1 to spikeIdx since we have a 0 ISI slot
-
-                elseif ~isempty(spikeIntValue) && length(spikeIntValue) > 1 % two spikes or more
+                    %                     spikeIntTable(nn, tt, curBinIdx, 1) = 1;
+                    
+                elseif length(spikeIdx) == 1 % 1 spike only -> treating it as though its a 0 spike interval
+                    spikeIntCount(nn, tt, curBinIdx) = {0};
+                    %                     spikeIntTable(nn, tt, curBinIdx, 1) = 1;
+                    
+                elseif ~isempty(spikeIntValue) % two spikes or more
                     spikeIntCount(nn, tt, curBinIdx) = {spikeIntValue};
-                    for ss = 1:length(spikeIntValue) % Count the number of times each spike interval occurs
-                        tmpspikeIntCount(nn, tt, curBinIdx, spikeIntValue(ss) + 1) = sum(spikeIntValue == spikeIntValue(ss));
-                    end
+                    %                     spikeIntTable(nn, tt, curBinIdx, :) = histcounts(spikeIntValue,  'BinEdges', -.5:snippetLength); % replaces the below lines of code
+                    %                     for ss = 1:length(spikeIntValue) % Count the number of times each spike interval occurs
+                    %                         spikeIntTable(nn, tt, curBinIdx, spikeIntValue(ss) + 1) = sum(spikeIntValue == spikeIntValue(ss));
+                    %                     end
                 end
                 curBinIdx = curBinIdx + 1;
-                
             end % bin loop
         end % rep loop
     end % texture loop
--- runMutualInfoAnalysis.m
+++ runMutualInfoAnalysis.m
@@ -33,25 +33,11 @@ saveDataFilename = './Data/infoCalculations_ISI_SpikeCount_IntervalISI_DistMetri
 startTime = 100; % ms
 poiDur = 1800; % Duration of period of interest (poi) in ms
 convFactor = 1000; % Conversion factor from bits/bin to bits/sec in MI calculations (1000 keeps the info in units of bits/bin)
-intTimeWindow = 5; %[1, 2, 5, 10, 20, 25, 50, 100]; % Time period (ms) over which a neuron can receive information (integration time)
 
 % Spike Train Params
 spikeTrainRes = 1; % Binning resolution of the spike train (ms)
-snippetLength = [10, 20, 50, 100]; % ms (NS 1/23/24: May remove later)
-
+snippetLength = [2:5]; %[2, 5, 10, 20, 50, 100]; % ms
 stParams.snippetLength = snippetLength; stParams.spikeTrainRes = spikeTrainRes;
-
-% ISI Params
-isiRes = 1; % ms
-isiCutoff = 120; %ms
-
-isiParams.resolution = isiRes; isiParams.cutoff = isiCutoff;
-
-% Tau Params
-isiTau = 1:15; % 1:19
-maxTauCount = [19,9,6,4,3,3,2,2,2,1,1,1,1,1,1]; % [9,4,3,2,1,1,1,1,1]; %
-
-isiParams.tau = isiTau; isiParams.maxTauRate = maxTauCount;
 
 %% Processing Loop
 % 1/23/24 Notes: Since not doing tip to tail stitching, need to think about
@@ -60,58 +46,33 @@ isiParams.tau = isiTau; isiParams.maxTauRate = maxTauCount;
 % calculateMutualInformation (since that already takes joint distributions
 % as input)
 
-%% ISI calculations using actual spike times (gonna keep it for now but might remove later)
-% Get ISI Data
-% isiEdges = 0:isiRes:isiCutoff; % 0:1:120
-% isiHist = getISIHist(cData, startTime, poiDur, isiRes, isiCutoff);
-%
-% for ll = 1:length(intTimeWindow) % This should be extended to the outer most loop (since we will do this for all the info calcs)
-%     clc; fprintf('ISI Res: %i/%i, ISI Length %i/%i\n',  length(isiRes), ll, length(intTimeWindow));
-%
-%     isiLimit = find(isiEdges <= intTimeWindow(ll), 1, 'last');
-%
-%     tmpISI = calculateMutualInformation(isiHist(:,:,1:isiLimit), NaN, convFactor);
-%     miNeural.ISI(ll, :) = tmpISI.mutualInfo;
-%     entNeural.ISI(ll, :) = tmpISI.entropy;
-%     jointNeural.ISI{ll} = tmpISI.jointCount;
-%     %         rate.ISI(ll, :, :) =
-%     %         [distMetric.ISI.pcDJS(ll, :, :, :), distMetric.ISI.pcRsq(ll, :)] = djsAnalysis(tmpISI.jointCount(find(pcIdx == 1), textIdx, :), humanScores, 0);
-%     %         [distMetric.ISI.saDJS(ll, :, :, :), distMetric.ISI.saRsq(ll, :)] = djsAnalysis(tmpISI.jointCount(find(saIdx == 1), textIdx, :), humanScores, 0);
+% Info calculations using binarized spike trains
+
+% spikeTrain = binSpikeData(cData, startTime, poiDur, spikeTrainRes);
+% 
+% for tt = 1:length(snippetLength)
+%     % Spike Count
+%     [spikeCount] = getSpikeCount(spikeTrain, snippetLength(tt)); % Outputs spike count not p(x|t)
+% 
+%     tmpSpikes = calculateMutualInformation(spikeCount, 0:snippetLength(tt), convFactor);
+%     miNeural.SpikeCount(tt, :) = tmpSpikes.mutualInfo;
+%     entNeural.SpikeCount(tt, :) = tmpSpikes.entropy;
+%     jointNeural.SpikeCount{tt} = tmpSpikes.jointCount;
+%     rate.SpikeCount(tt, :, :) = mean(spikeCount, 3);
+%     %         [distMetric.SpikeCount.pcDJS(sl, :, :, :), distMetric.SpikeCount.pcRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(pcIdx == 1), textIdx, :), humanScores, 1);
+%     %         [distMetric.SpikeCount.saDJS(sl, :, :, :), distMetric.SpikeCount.saRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(saIdx == 1), textIdx, :), humanScores, 0);
 % end
 
-%% Info calculations using binarized spike trains
-% TODO: Implement integration time window here
-
-spikeTrain = binSpikeData(cData, startTime, poiDur, spikeTrainRes);
-
-for tt = 1:length(intTimeWindow)
-
-    % Spike Count
-    [spikeCount] = getSpikeCount(spikeTrain, intTimeWindow(tt)); % Outputs spike count not p(x|t)
-
-    tmpSpikes = calculateMutualInformation(spikeCount, 0:intTimeWindow(tt), convFactor);
-    miNeural.SpikeCount(sl, :) = tmpSpikes.mutualInfo;
-    entNeural.SpikeCount(sl, :) = tmpSpikes.entropy;
-    jointNeural.SpikeCount{sl} = tmpSpikes.jointCount;
-    rate.SpikeCount(sl, :, :) = mean(spikeCount, 3);
-    %         [distMetric.SpikeCount.pcDJS(sl, :, :, :), distMetric.SpikeCount.pcRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(pcIdx == 1), textIdx, :), humanScores, 1);
-    %         [distMetric.SpikeCount.saDJS(sl, :, :, :), distMetric.SpikeCount.saRsq(sl, :)] = djsAnalysis(tmpSpikes.jointCount(find(saIdx == 1), textIdx, :), humanScores, 0);
-
-
+for tt = 1:length(snippetLength)
     % Spike Interval Code (was ISI Tau)
-    [spikeIntCount] = getSpikeIntervalCount(spikeTrain, intTimeWindow(tt));
-    
-    % How does max ISI count play a role here?
-
-
-    tmpSpikeInt = calculateMutualInformation(spikeIntCount, 0:maxTauCount(tt), convFactor);
-    miNeural.TauIsi(tt, :) = tmpSpikeInt.mutualInfo;
-    entNeural.TauIsi(tt, :) = tmpSpikeInt.entropy;
-    jointNeural.TauIsi{tt} = tmpSpikeInt.jointCount;
-    rate.isiTau(tt, :) = mean(squeeze(mean(spikeIntCount, 2)), 2);
-
+    [spikeIntCount] = getSpikeIntervalCount(spikeTrain, snippetLength(tt));
+    tmpSpikeInt = calculateMutualInformation(spikeIntCount, NaN, convFactor);
+    miSpikeInt(tt, :) = tmpSpikeInt.mutualInfo;
+    entSpikeInt(tt, :) = tmpSpikeInt.entropy;
+    jointSpikeInt{tt} = tmpSpikeInt.jointCount;
+    rateSpikeInt(tt, :) = mean(squeeze(mean(spikeIntCount, 2)), 2);
 end % integration time window loop
-
-if saveData
-    save('./Data/mostRecentCalculation.mat', 'miNeural', 'entNeural', 'jointNeural', 'distMetric', 'stParams', 'isiParams', 'neuronType', 'pcIdx', 'saIdx', 'isiHist', 'spikeTrain')
-end
+% 
+% if saveData
+%     save('./Data/mostRecentCalculation.mat', 'miNeural', 'entNeural', 'jointNeural', 'distMetric', 'stParams', 'isiParams', 'neuronType', 'pcIdx', 'saIdx', 'isiHist', 'spikeTrain')
+% end
