3ca0f798165b556f811660355a5306dc76015733
Natalya Shelchkova <nshelch@gmail.com>
2024-02-18 18:03:27 -0600

    new figure plots

--- newFigures.m
+++ newFigures.m
@@ -1,21 +1,17 @@
 neuronType = {cData.neuron(:).type};
 pcIdx = strcmp(neuronType, 'PC'); saIdx = strcmp(neuronType, 'SA');
 
+%% Info in bits/bin
+pcData = squeeze(miNeural.SpikeCount(:, pcIdx));
+pcSpikeCountMeans = mean(pcData, 2);
+pcSpikeCountStd = std(pcData, 0, 2);
 
-pcSpikeCountMeans = mean(squeeze(miNeural.SpikeCount(:, pcIdx)), 2);
-saSpikeCountMeans = mean(squeeze(miNeural.SpikeCount(:, saIdx)), 2);
-
-pcSpikeCountStd = std(squeeze(miNeural.SpikeCount(:, pcIdx)), 0, 2);
-saSpikeCountStd = std(squeeze(miNeural.SpikeCount(:, saIdx)), 0, 2);
-
-pcSpikeIntMeans = mean(squeeze(miNeural.SpikeInt(:, pcIdx)), 2);
-saSpikeIntMeans = mean(squeeze(miNeural.SpikeInt(:, saIdx)), 2);
-
-pcSpikeIntStd = std(squeeze(miNeural.SpikeInt(:, pcIdx)), 0, 2);
-saSpikeIntStd = std(squeeze(miNeural.SpikeInt(:, saIdx)), 0, 2);
+saData = squeeze(miNeural.SpikeCount(:, saIdx));
+saSpikeCountMeans = mean(saData, 2);
+saSpikeCountStd = std(saData, 0, 2);
 
 figure('WindowStyle', 'docked')
-subplot(2,1,1)
+subplot(3,1,1)
 errorbar(snippetLength, pcSpikeCountMeans, pcSpikeCountStd, '-o', ...
     'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
     'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
@@ -23,32 +19,305 @@ hold on
 errorbar(snippetLength, saSpikeCountMeans, saSpikeCountStd, '-o', ...
     'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
     'Color', rgb('Green'), 'LineWidth', 1.5)
-ylabel('Info [bits]')
+ylabel('Info [bits/bin]')
 set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
 box off
-% ylim([0 2])
+ylim([0 .225])
 legend({'PC', 'SA'}, 'Location', 'Northwest')
 title('Spike Count')
 
+pcData = squeeze(miNeural.SplitSpikeInt(:, pcIdx));
+pcSpikeIntSplitMeans = mean(pcData, 2);
+pcSpikeIntSplitStd = std(pcData, 0, 2);
+
+saData = squeeze(miNeural.SplitSpikeInt(:, saIdx));
+saSpikeIntSplitMeans = mean(saData, 2);
+saSpikeIntSplitStd = std(saData, 0, 2);
+
+subplot(3,1,2)
+errorbar(snippetLength, pcSpikeIntSplitMeans, pcSpikeIntSplitStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+hold on
+errorbar(snippetLength, saSpikeIntSplitMeans, saSpikeIntSplitStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
+ylabel('Info [bits/bin]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+ylim([0 .225])
+legend({'PC', 'SA'}, 'Location', 'Northwest')
+title('Split Spike Interval')
+
+pcData = squeeze(miNeural.Words(:, pcIdx));
+pcWordMeans = mean(pcData, 2);
+pcWordStd = std(pcData, 0, 2);
+
+saData = squeeze(miNeural.Words(:, saIdx));
+saWordMeans = mean(saData, 2);
+saWordStd = std(saData, 0, 2);
+
+subplot(3,1,3)
+errorbar(snippetLength, pcWordMeans, pcWordStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+hold on
+errorbar(snippetLength, saWordMeans, saWordStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
+ylabel('Info [bits/bin]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+ylim([0 .225])
+legend({'PC', 'SA'}, 'Location', 'Northwest')
+title('Words')
+xlabel('Snippet Length [ms]')
+
+%% Info in bits/bit - grouped by info measure
+
+pcData = squeeze(miNeural.SpikeCount(:, pcIdx)) ./ snippetLength';
+pcSpikeCountMeans = mean(pcData, 2);
+pcSpikeCountStd = std(pcData, 0, 2);
+
+saData = squeeze(miNeural.SpikeCount(:, saIdx)) ./ snippetLength';
+saSpikeCountMeans = mean(saData, 2);
+saSpikeCountStd = std(saData, 0, 2);
+
+figure('WindowStyle', 'docked')
+subplot(3,1,1)
+errorbar(snippetLength, pcSpikeCountMeans, pcSpikeCountStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+hold on
+errorbar(snippetLength, saSpikeCountMeans, saSpikeCountStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
+ylabel('Info [bits/bit]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+ylim([0 .025])
+legend({'PC', 'SA'}, 'Location', 'Northwest')
+title('Spike Count')
+
+pcData = squeeze(miNeural.SplitSpikeInt(:, pcIdx)) ./ snippetLength';
+pcSpikeIntSplitMeans = mean(pcData, 2);
+pcSpikeIntSplitStd = std(pcData, 0, 2);
+
+saData = squeeze(miNeural.SplitSpikeInt(:, saIdx)) ./ snippetLength';
+saSpikeIntSplitMeans = mean(saData, 2);
+saSpikeIntSplitStd = std(saData, 0, 2);
+
+subplot(3,1,2)
+errorbar(snippetLength, pcSpikeIntSplitMeans, pcSpikeIntSplitStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+hold on
+errorbar(snippetLength, saSpikeIntSplitMeans, saSpikeIntSplitStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
+ylabel('Info [bits/bit]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+ylim([0 .025])
+legend({'PC', 'SA'}, 'Location', 'Northwest')
+title('Split Spike Interval')
+
+pcData = squeeze(miNeural.Words(:, pcIdx)) ./ snippetLength';
+pcWordMeans = mean(pcData, 2);
+pcWordStd = std(pcData, 0, 2);
+
+saData = squeeze(miNeural.Words(:, saIdx)) ./ snippetLength';
+saWordMeans = mean(saData, 2);
+saWordStd = std(saData, 0, 2);
+
+subplot(3,1,3)
+errorbar(snippetLength, pcWordMeans, pcWordStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+hold on
+errorbar(snippetLength, saWordMeans, saWordStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
+ylabel('Info [bits/bit]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+ylim([0 .025])
+legend({'PC', 'SA'}, 'Location', 'Northwest')
+title('Words')
+xlabel('Snippet Length [ms]')
+
+
+%% Info in bits/bit - grouped by neuron
+
+spikeCountColor = rgb('MediumVioletRed');
+spikeIntColor = rgb('DeepSkyBlue');
+wordColor = rgb('SpringGreen');
+
+pcData = squeeze(miNeural.SpikeCount(:, pcIdx)) ./ snippetLength';
+pcSpikeCountMeans = mean(pcData, 2);
+pcSpikeCountStd = std(pcData, 0, 2);
+
+pcData = squeeze(miNeural.SplitSpikeInt(:, pcIdx)) ./ snippetLength';
+pcSpikeIntSplitMeans = mean(pcData, 2);
+pcSpikeIntSplitStd = std(pcData, 0, 2);
+
+pcData = squeeze(miNeural.Words(:, pcIdx)) ./ snippetLength';
+pcWordMeans = mean(pcData, 2);
+pcWordStd = std(pcData, 0, 2);
+
+figure('WindowStyle', 'docked')
+subplot(2,1,1)
+errorbar(snippetLength, pcSpikeCountMeans, pcSpikeCountStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeCountColor, 'MarkerFaceColor', spikeCountColor, ...
+    'Color', spikeCountColor, 'LineWidth', 1.5)
+hold on
+
+errorbar(snippetLength, pcSpikeIntSplitMeans, pcSpikeIntSplitStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeIntColor, 'MarkerFaceColor', spikeIntColor, ...
+    'Color', spikeIntColor, 'LineWidth', 1.5)
+
+errorbar(snippetLength, pcWordMeans, pcWordStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', wordColor, 'MarkerFaceColor', wordColor, ...
+    'Color', wordColor, 'LineWidth', 1.5)
+
+ylabel('Info [bits/bit]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+ylim([0 .025])
+legend({'Spike Count', 'Spike Interval', 'Words'}, 'Location', 'Northwest')
+title('PC Neurons')
+
+saData = squeeze(miNeural.SpikeCount(:, saIdx)) ./ snippetLength';
+saSpikeCountMeans = mean(saData, 2);
+saSpikeCountStd = std(saData, 0, 2);
+
+saData = squeeze(miNeural.SplitSpikeInt(:, saIdx)) ./ snippetLength';
+saSpikeIntSplitMeans = mean(saData, 2);
+saSpikeIntSplitStd = std(saData, 0, 2);
+
+saData = squeeze(miNeural.Words(:, saIdx)) ./ snippetLength';
+saWordMeans = mean(saData, 2);
+saWordStd = std(saData, 0, 2);
 
 subplot(2,1,2)
+errorbar(snippetLength, saSpikeCountMeans, saSpikeCountStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeCountColor, 'MarkerFaceColor', spikeCountColor, ...
+    'Color', spikeCountColor, 'LineWidth', 1.5)
+hold on
+
+errorbar(snippetLength, saSpikeIntSplitMeans, saSpikeIntSplitStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeIntColor, 'MarkerFaceColor', spikeIntColor, ...
+    'Color', spikeIntColor, 'LineWidth', 1.5)
+
+errorbar(snippetLength, saWordMeans, saWordStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', wordColor, 'MarkerFaceColor', wordColor, ...
+    'Color', wordColor, 'LineWidth', 1.5)
+
+ylabel('Info [bits/bit]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+ylim([0 .025])
+% legend({'Spike Count', 'Spike Interval', 'Words'}, 'Location', 'Northwest')
+title('SA Neurons')
+
+
+%% Splitting single spike labels
+
+pcSpikeIntSplitMeans = mean(squeeze(miNeural.SplitSpikeInt(:, pcIdx)), 2);
+pcSpikeIntMeans = mean(squeeze(miNeural.SpikeInt(:, pcIdx)), 2);
+
+pcSpikeIntStd = std(squeeze(miNeural.SpikeInt(:, pcIdx)), 0, 2);
+pcSpikeIntSplitStd = std(squeeze(miNeural.SplitSpikeInt(:, pcIdx)), 0, 2);
+
+figure('WindowStyle', 'docked')
+subplot(2,1,1)
+errorbar(snippetLength, pcSpikeIntSplitMeans, pcSpikeIntSplitStd, '--o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('White'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+hold on
 errorbar(snippetLength, pcSpikeIntMeans, pcSpikeIntStd, '-o', ...
     'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
     'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+
+ylabel('Info [bits/bin]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+% ylim([0 2])
+legend({'PC Split', 'PC'}, 'Location', 'Northwest')
+title('PC Spike Interval Count')
+
+saSpikeIntSplitMeans = mean(squeeze(miNeural.SplitSpikeInt(:, saIdx)), 2);
+saSpikeIntMeans = mean(squeeze(miNeural.SpikeInt(:, saIdx)), 2);
+
+saSpikeIntStd = std(squeeze(miNeural.SpikeInt(:, saIdx)), 0, 2);
+saSpikeIntSplitStd = std(squeeze(miNeural.SplitSpikeInt(:, saIdx)), 0, 2);
+
+subplot(2,1,2)
+errorbar(snippetLength, saSpikeIntSplitMeans, saSpikeIntSplitStd, '--o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('White'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
 hold on
 errorbar(snippetLength, saSpikeIntMeans, saSpikeIntStd, '-o', ...
     'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
     'Color', rgb('Green'), 'LineWidth', 1.5)
-ylabel('Info [bits]')
+ylabel('Info [bits/bin]')
 set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
 box off
 % ylim([0 2])
-legend({'PC', 'SA'}, 'Location', 'Northwest')
-title('Spike Interval')
+legend({'SA Split', 'SA'}, 'Location', 'Northwest')
+title('SA Spike Interval Count')
 xlabel('Snippet Length [ms]')
 
-% saveas(gcf, './Figures/SpikeCountVsIsi.png')
-% export_fig('./Figures/SpikeCountVsIsi.png', '-dpng', '-transparent', '-r300');
+%% Splitting single spike labels (bits/bit)
+
+pcData = squeeze(miNeural.SplitSpikeInt(:, pcIdx)) ./ snippetLength';
+pcSpikeIntSplitMeans = mean(pcData, 2);
+pcSpikeIntSplitStd = std(pcData, 0, 2);
+
+pcData = squeeze(miNeural.SpikeInt(:, pcIdx)) ./ snippetLength';
+pcSpikeIntMeans = mean(pcData, 2);
+pcSpikeIntStd = std(pcData, 0, 2);
+
+figure('WindowStyle', 'docked')
+subplot(2,1,1)
+errorbar(snippetLength, pcSpikeIntSplitMeans, pcSpikeIntSplitStd, '--o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('White'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+hold on
+errorbar(snippetLength, pcSpikeIntMeans, pcSpikeIntStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
+    'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
+
+ylabel('Info [bits/bit]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+% ylim([0 2])
+legend({'PC Split', 'PC'}, 'Location', 'Northwest')
+title('PC Spike Interval Count')
+
+saData = squeeze(miNeural.SplitSpikeInt(:, saIdx)) ./ snippetLength';
+saSpikeIntSplitMeans = mean(saData, 2);
+saSpikeIntSplitStd = std(saData, 0, 2);
+
+saData = squeeze(miNeural.SpikeInt(:, saIdx)) ./ snippetLength';
+saSpikeIntMeans = mean(saData, 2);
+saSpikeIntStd = std(saData, 0, 2);
+
+subplot(2,1,2)
+errorbar(snippetLength, saSpikeIntSplitMeans, saSpikeIntSplitStd, '--o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('White'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
+hold on
+errorbar(snippetLength, saSpikeIntMeans, saSpikeIntStd, '-o', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', rgb('Green'), 'MarkerFaceColor', rgb('Green'), ...
+    'Color', rgb('Green'), 'LineWidth', 1.5)
+ylabel('Info [bits/bit]')
+set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
+box off
+% ylim([0 2])
+legend({'SA Split', 'SA'}, 'Location', 'Northwest')
+title('SA Spike Interval Count')
+xlabel('Snippet Length [ms]')
 
 %% % Scatter plots
 % Size of dot = snippet size
