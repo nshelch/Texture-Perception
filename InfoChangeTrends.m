28e71675241229daaa9cece06073b68090266862
Office <nshelch@gmail.com>
2024-03-11 12:56:12 -0500

    Added figures

--- InfoChangeTrends.m
+++ InfoChangeTrends.m
@@ -37,30 +37,149 @@ wordColor = rgb('SpringGreen');
 
 %% Change in info due to snippet length
 
-pcData = diff(squeeze(miNeural.SpikeCount(:, pcIdx)) ./ snippetLength');
-
-pcDeltaWords = mean(pcData, 2);
-pcDeltaWordsStd = std(pcData, 0, 2);
-
 figure('WindowStyle', 'docked')
-subplot(2,1,1)
-errorbar(pcDeltaWords, pcDeltaWordsStd, '--o', ...
-    'MarkerSize', 10, 'MarkerEdgeColor', pcColor, 'MarkerFaceColor', rgb('White'), ...
-    'Color', pcColor, 'LineWidth', 1.5)
-hold on
-% errorbar(snippetLength, pcSpikeIntMeans, pcSpikeIntStd, '-o', ...
-%     'MarkerSize', 10, 'MarkerEdgeColor', rgb('DarkOrange'), 'MarkerFaceColor', rgb('DarkOrange'), ...
-%     'Color', rgb('DarkOrange'), 'LineWidth', 1.5)
-% 
-% ylabel('Info [bits/bit]')
-% set(gca, 'XLim', [1.5 10.5], 'XTick', snippetLength, 'FontSize', 12)
-% box off
-% % ylim([0 2])
-% legend({'PC Split', 'PC'}, 'Location', 'Northwest')
-% title('PC Spike Interval Count')
+subplot(2,1,1); hold on;
+
+pcData = diff(squeeze(miNeural.SpikeCount(:, pcIdx)) ./ snippetLength');
+pcMean = mean(pcData, 2);
+pcStd = std(pcData, 0, 2);
+
+errorbar(pcMean, pcStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeCountColor, 'MarkerFaceColor', spikeCountColor, ...
+    'Color', spikeCountColor, 'LineWidth', 1.5)
+
+pcData = diff(squeeze(miNeural.SpikeInt(:, pcIdx)) ./ snippetLength');
+pcMean = mean(pcData, 2);
+pcStd = std(pcData, 0, 2);
+
+errorbar(pcMean, pcStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeIntColor, 'MarkerFaceColor', spikeIntColor, ...
+    'Color', spikeIntColor, 'LineWidth', 1.5)
+
+pcData = diff(squeeze(miNeural.Words(:, pcIdx)) ./ snippetLength');
+pcMean = mean(pcData, 2);
+pcStd = std(pcData, 0, 2);
+
+errorbar(pcMean, pcStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', wordColor, 'MarkerFaceColor', wordColor, ...
+    'Color', wordColor, 'LineWidth', 1.5)
+
+ylabel('\DeltaInfo [bits/bit]')
+xTickLabel = {'2 \rightarrow 3', '3 \rightarrow 4', '4 \rightarrow 5', '5 \rightarrow 6', '6 \rightarrow 8', '8 \rightarrow 10'};
+set(gca, 'XLim', [0.5 6.5], 'XTick', 1:6, 'XTickLabel', xTickLabel, 'FontSize', 12)
+box off
+text(.75, .00225, ColorText({'Spike Count', 'Spike Int.', 'Words'}, [spikeCountColor; spikeIntColor; wordColor]), ...
+    'FontSize', 12, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top')
+title('PC Neurons')
+
+subplot(2,1,2); hold on;
+
+saData = diff(squeeze(miNeural.SpikeCount(:, saIdx)) ./ snippetLength');
+saMean = mean(saData, 2);
+saStd = std(saData, 0, 2);
+
+errorbar(mean(saData, 2), std(saData, 0, 2), 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeCountColor, 'MarkerFaceColor', spikeCountColor, ...
+    'Color', spikeCountColor, 'LineWidth', 1.5)
 
 
+saData = diff(squeeze(miNeural.SpikeInt(:, saIdx)) ./ snippetLength');
+saMean = mean(saData, 2);
+saStd = std(saData, 0, 2);
 
+errorbar(saMean, saStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeIntColor, 'MarkerFaceColor', spikeIntColor, ...
+    'Color', spikeIntColor, 'LineWidth', 1.5)
 
+saData = diff(squeeze(miNeural.Words(:, saIdx)) ./ snippetLength');
+saMean = mean(saData, 2);
+saStd = std(saData, 0, 2);
+
+errorbar(saMean, saStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', wordColor, 'MarkerFaceColor', wordColor, ...
+    'Color', wordColor, 'LineWidth', 1.5)
+
+ylabel('\DeltaInfo [bits/bit]')
+xlabel('Snippet Length [ms]')
+xTickLabel = {'2 \rightarrow 3', '3 \rightarrow 4', '4 \rightarrow 5', '5 \rightarrow 6', '6 \rightarrow 8', '8 \rightarrow 10'};
+set(gca, 'XLim', [0.5 6.5], 'XTick', 1:6, 'XTickLabel', xTickLabel, 'FontSize', 12)
+box off
+text(.75, .00095, ColorText({'Spike Count', 'Spike Int.', 'Words'}, [spikeCountColor; spikeIntColor; wordColor]), ...
+    'FontSize', 12, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top')
+title('SA Neurons')
 
 %% Change in info due to label
+
+figure('WindowStyle', 'docked')
+subplot(3,1,1); hold on;
+
+pcData = diff(squeeze(miNeural.SpikeCount(:, pcIdx)) ./ snippetLength');
+pcMean = mean(pcData, 2);
+pcStd = std(pcData, 0, 2);
+
+errorbar(pcMean, pcStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', pcColor, 'MarkerFaceColor', pcColor, ...
+    'Color', pcColor, 'LineWidth', 1.5)
+
+saData = diff(squeeze(miNeural.SpikeCount(:, saIdx)) ./ snippetLength');
+saMean = mean(saData, 2);
+saStd = std(saData, 0, 2);
+
+errorbar(mean(saData, 2), std(saData, 0, 2), 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeCountColor, 'MarkerFaceColor', spikeCountColor, ...
+    'Color', spikeCountColor, 'LineWidth', 1.5)
+
+ylabel('\DeltaInfo [bits/bit]')
+xTickLabel = {'2 \rightarrow 3', '3 \rightarrow 4', '4 \rightarrow 5', '5 \rightarrow 6', '6 \rightarrow 8', '8 \rightarrow 10'};
+set(gca, 'XLim', [0.5 6.5], 'XTick', 1:6, 'XTickLabel', xTickLabel, 'FontSize', 12)
+box off
+text(.75, .00225, ColorText({'Spike Count', 'Spike Int.', 'Words'}, [spikeCountColor; spikeIntColor; wordColor]), ...
+    'FontSize', 12, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top')
+title('PC Neurons')
+
+subplot(3,1,2); hold on;
+
+pcData = diff(squeeze(miNeural.SpikeInt(:, pcIdx)) ./ snippetLength');
+pcMean = mean(pcData, 2);
+pcStd = std(pcData, 0, 2);
+
+errorbar(pcMean, pcStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeIntColor, 'MarkerFaceColor', spikeIntColor, ...
+    'Color', spikeIntColor, 'LineWidth', 1.5)
+
+
+saData = diff(squeeze(miNeural.SpikeInt(:, saIdx)) ./ snippetLength');
+saMean = mean(saData, 2);
+saStd = std(saData, 0, 2);
+
+errorbar(saMean, saStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeIntColor, 'MarkerFaceColor', spikeIntColor, ...
+    'Color', spikeIntColor, 'LineWidth', 1.5)
+
+subplot(3,1,3); hold on;
+
+
+pcData = diff(squeeze(miNeural.SpikeInt(:, pcIdx)) ./ snippetLength');
+pcMean = mean(pcData, 2);
+pcStd = std(pcData, 0, 2);
+
+errorbar(pcMean, pcStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', spikeIntColor, 'MarkerFaceColor', spikeIntColor, ...
+    'Color', spikeIntColor, 'LineWidth', 1.5)
+
+saData = diff(squeeze(miNeural.Words(:, saIdx)) ./ snippetLength');
+saMean = mean(saData, 2);
+saStd = std(saData, 0, 2);
+
+errorbar(saMean, saStd, 'o-', ...
+    'MarkerSize', 10, 'MarkerEdgeColor', wordColor, 'MarkerFaceColor', wordColor, ...
+    'Color', wordColor, 'LineWidth', 1.5)
+
+ylabel('\DeltaInfo [bits/bit]')
+xlabel('Snippet Length [ms]')
+xTickLabel = {'2 \rightarrow 3', '3 \rightarrow 4', '4 \rightarrow 5', '5 \rightarrow 6', '6 \rightarrow 8', '8 \rightarrow 10'};
+set(gca, 'XLim', [0.5 6.5], 'XTick', 1:6, 'XTickLabel', xTickLabel, 'FontSize', 12)
+box off
+text(.75, .00095, ColorText({'Spike Count', 'Spike Int.', 'Words'}, [spikeCountColor; spikeIntColor; wordColor]), ...
+    'FontSize', 12, 'HorizontalAlignment', 'left', 'VerticalAlignment', 'top')
+title('SA Neurons')
